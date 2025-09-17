31e0b7ff83754c17c911566bee103b691914f2ac
armannm <anm4500@gmail.com>
2025-09-17 23:52:24 +0330

    init new project

---
+++ src/JobFinder.Client/src/app/app.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AppService } from './app.service';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, JsonPipe],
+  providers: [AppService],
+  template: `
+    <button (click)="getWeather()">get weather</button>
+    <h1>{{ weather | json }}</h1>
+
+    <router-outlet />
+  `,
+  styles: [],
+})
+export class App {
+  protected readonly title = signal('JobFinder.Client');
+  weather: any;
+
+  constructor(private service: AppService) {
+  }
+
+  getWeather() {
+    this.service.getWeather().subscribe((res) => {
+      this.weather = res;
+    })
+  }
+}
