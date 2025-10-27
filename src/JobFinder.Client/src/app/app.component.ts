8bbb09a9fd1b2d6e85cb4c503b1025459209148f
armannm <anm4500@gmail.com>
2025-10-27 09:46:41 +0330

    fix css issues

--- src/JobFinder.Client/src/app/app.component.ts
+++ src/JobFinder.Client/src/app/app.component.ts
@@ -8,16 +8,18 @@ import { BottomNavbarComponent } from "./bottom-navbar/bottom-navbar.component";
   selector: 'app-root',
   imports: [RouterOutlet, MenubarModule, BottomNavbarComponent],
   template: `
-    <p-menubar>
-      <ng-template #start>
-        <div class="top-bar">
-          <h3>شغل یاب</h3>
-          <p>موتور جستجوی مشاغل از وبسایت های کاریابی</p>
-        </div>
-      </ng-template>
-    </p-menubar>
-    <router-outlet></router-outlet>
-    <app-bottom-navbar></app-bottom-navbar>
+      <p-menubar>
+        <ng-template #start>
+          <div class="top-bar">
+            <h3>شغل یاب</h3>
+            <p>موتور جستجوی مشاغل از وبسایت های کاریابی</p>
+          </div>
+        </ng-template>
+      </p-menubar>
+      <div class="my-content">
+        <router-outlet class="content-container"></router-outlet>
+      </div>
+      <app-bottom-navbar></app-bottom-navbar>
   `,
 })
 export class AppComponent {
--- src/JobFinder.Client/src/app/bottom-navbar/bottom-navbar.component.ts
+++ src/JobFinder.Client/src/app/bottom-navbar/bottom-navbar.component.ts
@@ -7,38 +7,6 @@ import { TabMenuModule } from 'primeng/tabmenu';
   imports: [TabMenuModule, RouterModule],
   templateUrl: './bottom-navbar.component.html',
   encapsulation: ViewEncapsulation.None,
-  styles: [`
-    .bottom-navbar {
-      position: sticky;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      height: 56px;
-      background-color: #fff;
-      border-top: 1px solid #ddd;
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
-      z-index: 1000;
-    }
-
-    .nav-item {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      text-decoration: none;
-      color: #555;
-      font-size: 12px;
-    }
-
-    .nav-item i {
-      font-size: 1.4rem;
-    }
-
-    .nav-item.active {
-      color: #007ad9; /* PrimeNG primary color */
-    }
-  `]
 })
 export class BottomNavbarComponent {
 
